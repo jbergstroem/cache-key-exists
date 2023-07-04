ace6902e6be7b86dcdcb1eed6fab723012bca52d
Johan Bergström <bugs@bergstroem.nu>
2023-07-04 17:18:26 -0400

    feat: unit tests
    Also, fix any known warnings from linters and remove `validate.sh`.

--- test/unit.sh
+++ test/unit.sh
@@ -1,56 +1,93 @@
 #!/usr/bin/env bash_unit
 
-mock_cache_hit='{
-  "total_count": 3,
-  "actions_caches": [
-    {
-      "id": 2268,
-      "ref": "refs/heads/main",
-      "key": "Linux-node-deps-66c80c704b22f55d06ef1ad78525ab309593e84848c4d8e9718d1edadbd6f811",
-      "version": "25e65b70d167147605b4332eb57b6d34df8e0303b88c4de252efb18cf3ed148a",
-      "last_accessed_at": "2023-06-30T21:40:29.876666700Z",
-      "created_at": "2023-06-30T14:02:52.896666700Z",
-      "size_in_bytes": 151366377
-    },
-    {
-      "id": 2271,
-      "ref": "refs/pull/15/merge",
-      "key": "Linux-node-deps-66c80c704b22f55d06ef1ad78525ab309593e84848c4d8e9718d1edadbd6f811",
-      "version": "25e65b70d167147605b4332eb57b6d34df8e0303b88c4de252efb18cf3ed148a",
-      "last_accessed_at": "2023-06-30T15:15:22.510000000Z",
-      "created_at": "2023-06-30T14:30:26.450000000Z",
-      "size_in_bytes": 151376214
-    },
-    {
-      "id": 2265,
-      "ref": "refs/pull/19/merge",
-      "key": "Linux-node-deps-66c80c704b22f55d06ef1ad78525ab309593e84848c4d8e9718d1edadbd6f811",
-      "version": "25e65b70d167147605b4332eb57b6d34df8e0303b88c4de252efb18cf3ed148a",
-      "last_accessed_at": "2023-06-30T13:38:51.193333300Z",
-      "created_at": "2023-06-30T02:49:20.306666700Z",
-      "size_in_bytes": 151357767
-    }
-  ]
-}'
-mock_cache_miss='{
-  "total_count": 0,
-  "actions_caches": []
-}'
-
-mock_invalid_repo='{
-  "message": "Not Found",
-  "documentation_url": "https://docs.github.com/rest/actions/cache#list-github-actions-caches-for-a-repository"
-}'
+# shellcheck source=test/fixtures/curl_responses.sh
+. fixtures/curl_responses.sh
 
 # shellcheck source=lib/main.sh
 . ../lib/main.sh
 # shellcheck source=lib/curl.sh
 . ../lib/curl.sh
-# shellcheck source=lib/validate.sh
-. ../lib/validate.sh
+
+test_should_output_versions() {
+  local retval
+  retval="$(run)"
+  assert_matches ".*Using bash.*" "$retval"
+  assert_matches ".*Using curl.*" "$retval"
+  assert_matches ".*Using jq.*" "$retval"
+}
+
+test_should_fail_on_missing_key() {
+  local retval
+  retval="$(run)"
+  assert_status_code 1 run
+  assert_matches ".*Missing cache key. Set it and try again." "$retval"
+}
+
+test_should_fail_on_missing_token() {
+  export key="foo"
+  local retval
+  retval="$(run)"
+  assert_status_code 1 run
+  assert_matches ".*Missing token for authentication. Set it and try again." "$retval"
+  unset key
+}
+
+test_should_fail_on_missing_repository() {
+  export key="foo" token="bar"
+  local retval
+  retval="$(run)"
+  assert_status_code 1 run
+  assert_matches ".*Missing repository to check cache for. Set it and try again." "$retval"
+  unset key token
+}
 
 test_validate_fail_exit() {
   assert "validate_fail_exit true"
   assert "validate_fail_exit false"
   assert_fail "validate_fail_exit ok"
+}
+
+test_cache_hit() {
+  export -f mock_cache_hit
+  fake run_curl mock_cache_hit
+  export key="foo" repository="test/repo" token="temp"
+  local retval
+  retval="$(run)"
+  assert_status_code 0 run
+  assert_matches ".*found a cache.*" "$retval"
+  unset key token repository mock_cache_hit
+}
+
+test_cache_miss() {
+  export -f mock_cache_miss
+  fake run_curl mock_cache_miss
+  export key="foo" repository="test/repo" token="temp"
+  local retval
+  retval="$(run)"
+  assert_status_code 0 run
+  assert_matches ".*could not find a cache.*" "$retval"
+  unset key token repository mock_cache_miss
+}
+
+test_cache_miss_with_exit_fail() {
+  export -f mock_cache_miss
+  fake run_curl mock_cache_miss
+  export key="foo" repository="test/repo" token="temp"
+  export fail_exit=true
+  local retval
+  retval="$(run)"
+  assert_status_code 1 run
+  assert_matches ".*could not find a cache.*" "$retval"
+  unset key token repository fail_exit mock_cache_miss
+}
+
+test_invalid_repository() {
+  export -f mock_invalid_repo
+  fake run_curl mock_invalid_repo
+  export key="foo" repository="test/repo" token="temp"
+  local retval
+  retval="$(run)"
+  assert_status_code 0 run
+  assert_matches ".*could not retrieve cache results: Not Found.*" "$retval"
+  unset key token repository mock_invalid_repo
 }
